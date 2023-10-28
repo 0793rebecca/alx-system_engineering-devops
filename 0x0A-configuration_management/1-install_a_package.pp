bf97123228122e54c8efc4ebb1786a1cf4e97347
0793rebecca <rebeccafrempongmensah@gmail.com>
2023-10-28 00:17:27 -0700

    Puppet, install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
-# install a specific version of flask (2.1.0)
+# Install a specific version of Flask (2.1.0)
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
 }
